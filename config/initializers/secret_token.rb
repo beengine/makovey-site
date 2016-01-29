e5dbc95b8ea1dcccc8b0cd9680fb8955d5acbd5e
Andrii <beeengine@gmail.com>
2016-01-29 21:43:24 +0200

    Just created project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MakoveySite::Application.config.secret_key_base = '0c3123a30cde73cf4d4ad5c7ceb4d85476e495e68df6cd3d88d1608915895116e9a6bcac81b333a67adbc1e6d9f74d26f35be1d91b7b395cf383ccc655cb6c1b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MakoveySite::Application.config.session_store :cookie_store, key: '_makovey-site_session'
