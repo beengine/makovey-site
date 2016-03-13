917f3b266b312086e0d8a96464d91b6c8ee5fd60
Andrii <beeengine@gmail.com>
2016-03-13 21:31:39 +0200

    fff

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,6 +28,8 @@ MakoveySite::Application.configure do
 
   # Generate digests for assets URLs.
   config.assets.digest = true
+  config.action_mailer.default_url_options = { :host => 'intense-peak-28279.herokuapp.com' }
+
 
   # Version of your assets, change this if you want to expire all your assets.
   config.assets.version = '1.0'
