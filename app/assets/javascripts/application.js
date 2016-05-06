a917e1824bf96629319e9cca24cf62cc864a8a8e
Andrii <beeengine@gmail.com>
2016-05-06 16:31:05 +0300

    Optimize display resolutions

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -25,7 +25,7 @@
 
 	skel.breakpoints({
 		xlarge:	'(max-width: 1680px)',
-		large:	'(max-width: 1280px)',
+		large:	'(max-width: 1240px)',
 		medium:	'(max-width: 980px)',
 		small:	'(max-width: 736px)',
 		xsmall:	'(max-width: 480px)'
