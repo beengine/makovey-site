c2509d6f169fea8543df1dbd59b4608a682ac965
Andrii <beeengine@gmail.com>
2016-05-07 14:48:04 +0300

    ending cloudinary #2

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
 	def photo_is_empty_side(post)
 		unless post.post_photo.blank?
-      content_tag :a, image_tag(post.post_photo), class: "image", href: post_path(post)
+      content_tag :a, cl_image_tag(post.post_photo), class: "image", href: post_path(post)
 		end
 	end
 end
