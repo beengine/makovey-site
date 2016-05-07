36cf16abe2cf107775edd957db5ba0d6a5393ee3
Andrii <beeengine@gmail.com>
2016-05-07 14:49:44 +0300

    ending cloudinary #3

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,7 +1,7 @@
 module PostsHelper
   def photo_is_empty(post)
     unless post.post_photo.blank?
-    content_tag :a, image_tag(post.post_photo), class: "image featured"
+    content_tag :a, cl_image_tag(post.post_photo), class: "image featured"
     end
   end
   def snd_title_is_empty(post)
